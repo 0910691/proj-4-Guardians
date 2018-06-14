1f338c0da5d33bb95750af028ff9fcc182999c10
Skillerde6de <0948157@hr.nl>
2018-06-14 11:25:53 +0200

    Balk om knop heen

--- Proj-4-Guardians/Resources/Resource.Designer.cs
+++ Proj-4-Guardians/Resources/Resource.Designer.cs
@@ -1810,70 +1810,73 @@ namespace Proj_4_Guardians
 			public const int ImageBackground = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int infobutton = 2130837593;
+			public const int ImgBack = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int maps = 2130837594;
+			public const int infobutton = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int Menu = 2130837595;
+			public const int maps = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int notification_action_background = 2130837596;
+			public const int Menu = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int notification_bg = 2130837597;
+			public const int notification_action_background = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_bg_low = 2130837598;
+			public const int notification_bg = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int notification_bg_low_normal = 2130837599;
+			public const int notification_bg_low = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int notification_bg_low_pressed = 2130837600;
+			public const int notification_bg_low_normal = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int notification_bg_normal = 2130837601;
+			public const int notification_bg_low_pressed = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int notification_bg_normal_pressed = 2130837602;
+			public const int notification_bg_normal = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int notification_icon_background = 2130837603;
-			
-			// aapt resource value: 0x7f02006d
-			public const int notification_template_icon_bg = 2130837613;
-			
-			// aapt resource value: 0x7f02006e
-			public const int notification_template_icon_low_bg = 2130837614;
+			public const int notification_bg_normal_pressed = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int notification_tile_bg = 2130837604;
+			public const int notification_icon_background = 2130837604;
+			
+			// aapt resource value: 0x7f02006e
+			public const int notification_template_icon_bg = 2130837614;
+			
+			// aapt resource value: 0x7f02006f
+			public const int notification_template_icon_low_bg = 2130837615;
 			
 			// aapt resource value: 0x7f020065
-			public const int notify_panel_notification_icon_bg = 2130837605;
+			public const int notification_tile_bg = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int Paper = 2130837606;
+			public const int notify_panel_notification_icon_bg = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int QuestionMark = 2130837607;
+			public const int Paper = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int Recycle = 2130837608;
+			public const int QuestionMark = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int RoundButton = 2130837609;
+			public const int Recycle = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int RoundImageButton = 2130837610;
+			public const int RoundButton = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int tooltip_frame_dark = 2130837611;
+			public const int RoundImageButton = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int tooltip_frame_light = 2130837612;
+			public const int tooltip_frame_dark = 2130837612;
+			
+			// aapt resource value: 0x7f02006d
+			public const int tooltip_frame_light = 2130837613;
 			
 			static Drawable()
 			{
